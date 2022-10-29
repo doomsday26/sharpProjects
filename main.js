c810de12db33b7234de6174d3ae0e541909f2899
doomsday26 <harshnamdeo261999@gmail.com>
2022-10-29 14:11:05 +0530

    index

---
+++ main.js
@@ -0,0 +1,11 @@
+let items=document.getElementsByClassName('list-group-item');
+// console.log(items[0].textContent="first item");
+for (let i = 0; i < items.length; i++) {
+  items[i].style.backgroundColor="gray"
+  
+}
+
+//1)at the top there will be a big heading of "Title" in place of <title></title>, then we have two input fields which are text and submit button 
+//document.getElementById("main-header").style.borderBottom="2px solid black"
+// document.getElementsByClassName('title')[0].style.color="green"
+// document.getElementsByClassName('title')[0].style.fontWeight="bold"
